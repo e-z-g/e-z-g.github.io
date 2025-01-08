91fcc22ec3b21143209f30b45b30b9799bc8acfa
e-z-g <samlloyd7@gmail.com>
2025-01-08 06:24:59 -0500

    Create dictionary.js

---
+++ dictionary.js
@@ -0,0 +1,15 @@
+// Common English words dictionary (abbreviated version)
+const DICTIONARY = [
+    // 3 letters
+    "THE", "AND", "FOR", "ARE", "BUT", "NOT", "YOU", "ALL", "ANY", "CAN", "HAD", "HER", "WAS", "ONE", "OUR", "OUT", "DAY", "GET", "HAS", "HIM", "HIS", "HOW", "MAN", "NEW", "NOW", "OLD", "SEE", "TWO", "WAY", "WHO", "BOY", "DID", "ITS", "LET", "PUT", "SAY", "SHE", "TOO", "USE",
+    // 4 letters
+    "THAT", "WITH", "HAVE", "THIS", "WILL", "YOUR", "FROM", "THEY", "KNOW", "WANT", "BEEN", "GOOD", "MUCH", "SOME", "TIME", "VERY", "WHEN", "COME", "HERE", "JUST", "LIKE", "LONG", "MAKE", "MANY", "MORE", "ONLY", "OVER", "SUCH", "TAKE", "THAN", "THEM", "WELL", "WERE",
+    // 5 letters
+    "ABOUT", "COULD", "WHICH", "THEIR", "OTHER", "THESE", "FIRST", "WOULD", "THERE", "WHERE", "AFTER", "RIGHT", "THINK", "THREE", "YEARS", "PLACE", "SOUND", "GREAT", "AGAIN", "STILL", "EVERY", "SMALL", "FOUND", "THOSE", "NEVER", "UNDER", "MIGHT", "WHILE", "HOUSE", "WORLD", "BELOW",
+    // 6 letters
+    "ALMOST", "AROUND", "BECAME", "BEFORE", "BETTER", "BETWEEN", "DURING", "MOBILE", "NUMBER", "SECOND", "SHOULD", "SIMPLE", "SYSTEM", "TOWARD", "CHANGE", "FAMILY", "FRIEND", "PEOPLE", "SCHOOL", "STRONG", "PUZZLE", "CODING",
+    // 7 letters
+    "ANOTHER", "BECAUSE", "BELIEVE", "BROUGHT", "PICTURE", "PRESENT", "SPECIAL", "THROUGH", "WHETHER", "WRITTEN", "MASTER",
+    // 8 letters
+    "CHILDREN", "DIFFERENT", "IMPORTANT", "QUESTION", "SENTENCE", "TOGETHER", "TRAINING"
+];
